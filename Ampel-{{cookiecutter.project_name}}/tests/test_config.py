09ca93eaaa97758b56ad411f9ef633a4226caeb9
Jakob van Santen <jvansanten@gmail.com>
2025-02-25 09:37:28 +0100

    initial commit

---
+++ Ampel-{{cookiecutter.project_name}}/tests/test_config.py
@@ -0,0 +1,13 @@
+from ampel.config.builder.DisplayOptions import DisplayOptions
+from ampel.config.builder.DistConfigBuilder import DistConfigBuilder
+
+
+def test_build_config() -> None:
+    cb = DistConfigBuilder(DisplayOptions(verbose=True, debug=True))
+    cb.load_distributions()
+    config = cb.build_config(
+        stop_on_errors=2,
+        config_validator="ConfigValidator",
+        get_unit_env=False,
+    )
+    assert config is not None
